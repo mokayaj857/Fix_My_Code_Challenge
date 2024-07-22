a63bcf8486c11d86dcc3db24a0ab30f2f3183762
john mokaya <mokayaj857@gmail.com>
2024-07-22 14:20:52 +0300

    Create square.py

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,23 @@
+class Square:
+    
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def perimeter_of_my_square(self):
+        """ Perimeter of the square """
+        return self.width * 4
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.perimeter_of_my_square())
